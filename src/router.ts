b55004bd89e0afd8155b55e3d69700657e114531
Revoltzin <jat1963@gmail.com>
2026-05-04 17:20:13 -0300

    list category added as a function to complement list categories route

--- src/router.ts
+++ src/router.ts
@@ -1,12 +1,11 @@
 import { Router } from 'express'
+import { listCategories } from './app/useCases/category/listCategories.js'
 
 export const router = Router()
 
 // List categories
 
-router.get('/categories', (req, res) => {
-    res.send('OK')
-})
+router.get('/categories', listCategories)
 
 // Create category
 
