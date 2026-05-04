0b0dc33436c2721513f7eedc623c01a4cfb591e1
Revoltzin <jat1963@gmail.com>
2026-05-04 17:10:32 -0300

    All routes created

---
+++ src/router.ts
@@ -0,0 +1,57 @@
+import { Router } from 'express'
+
+export const router = Router()
+
+// List categories
+
+router.get('/categories', (req, res) => {
+    res.send('OK')
+})
+
+// Create category
+
+router.post('/categories', (req, res) => {
+    res.send('OK')
+})
+
+// list products
+
+router.get('/products', (req, res) => {
+    res.send('OK')
+})
+
+// Create product
+
+router.post('/products', (req, res) => {
+    res.send('OK')
+})
+
+// get products by category
+
+router.get('/categories/:categoryId/products', (req, res) => {
+    res.send('OK')
+})
+
+// List orders
+
+router.get('/orders', (req, res) => {
+    res.send('OK')
+})
+
+// Create order
+
+router.post('/orders', (req, res) => {
+    res.send('OK')
+})
+
+// change order status
+
+router.patch('/orders/:orderId', (req, res) => {
+    res.send('OK')
+})
+
+// Delete / Cancel order
+
+router.delete('/orders/:orderId', (req, res) => {
+    res.send('OK')
+})
