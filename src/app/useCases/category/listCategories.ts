73b3f738072f38ade954e45afd5ae5b99cc54858
Revoltzin <jat1963@gmail.com>
2026-05-06 18:11:57 -0300

    ListCategory created and working

--- src/app/useCases/category/listCategories.ts
+++ src/app/useCases/category/listCategories.ts
@@ -1,5 +1,9 @@
 import type { Request, Response } from "express";
 
-export function listCategories (req: Request, res: Response) {
+import { Category } from "../../models/Category.js";
 
+export async function listCategories (req: Request, res: Response) {
+  const categories = await Category.find()
+
+  res.json(categories)
 }
