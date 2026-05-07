ed8e4c3a467660bff676bf8c3b64db7d06af9210
Revoltzin <jat1963@gmail.com>
2026-05-07 16:51:30 -0300

    Mongoose connect structure fixed

--- src/index.ts
+++ src/index.ts
@@ -8,11 +8,11 @@ const port = 3000
 app.use(express.json())
 app.use(router)
 
-app.listen(port, () => {
-  console.log(`🎸 Server is running on http://localhost:${port}`)
-})
-
-// MongoDB separado
-mongoose.connect('mongodb://localhost:27017')
-  .then(() => console.log('✅ MongoDB conectado'))
+mongoose.connect('mongodb://localhost:27017/waiterapp')
+  .then(() => {
+    console.log('✅ MongoDB conectado')
+    app.listen(port, () => {
+      console.log(`🎸 Server is running on http://localhost:${port}`)
+    })
+  })
   .catch((err) => console.log('❌ Erro ao conectar no MongoDB:', err.message))
