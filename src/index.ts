0cb9121f73e0c6392ac7294aa204faaa0ad056d3
Revoltzin <jat1963@gmail.com>
2026-05-19 22:31:40 -0300

    Cors added and implemented

--- src/index.ts
+++ src/index.ts
@@ -3,6 +3,8 @@ import { fileURLToPath } from 'url'
 import express from 'express'
 import mongoose from 'mongoose'
 import { router } from './router.js'
+import cors from 'cors'
+
 
 const __filename = fileURLToPath(import.meta.url)
 const __dirname = path.dirname(__filename)
@@ -11,6 +13,8 @@ const app = express()
 const port = 3000
 
 app.use('/uploads', express.static(path.resolve(__dirname, '..', 'uploads')))
+
+app.use(cors())
 app.use(express.json())
 app.use(router)
 
